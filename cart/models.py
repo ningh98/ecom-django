97687f04a6801a375be1698054439881ea1cf8ab
Ninghui Cai <ninghuicai@Ninghuis-MBP.attlocal.net>
2023-10-05 23:49:28 -0700

    Product list view

--- cart/models.py
+++ cart/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 User = get_user_model()
 
@@ -27,7 +29,8 @@ class Address(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=150)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='product_images')
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -82,3 +85,10 @@ class Payment(models.Model):
         @property
         def reference_number(self):
             return f"PAYMENT-{self.order}-{self.pk}"
+
+
+def pre_save_product_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
+pre_save.connect(pre_save_product_receiver, sender=Product)
