2634da04eab1188f472ff27fb60cf67d9a8638e6
Ninghui Cai <ninghuicai@Ninghuis-MBP.attlocal.net>
2023-10-06 17:05:30 -0700

    Product detail view

--- cart/views.py
+++ cart/views.py
@@ -7,3 +7,7 @@ from .models import Product
 class ProductListView(generic.ListView):
     template_name = 'cart/product_list.html'
     queryset = Product.objects.all()
+
+class ProductDetailView(generic.DetailView):
+    template_name = 'cart/product_detail.html'
+    queryset = Product.objects.all()
