bacf8bba2b40d604ba39fbc5cf862ab854ec7986
Ninghui Cai <ninghuicai@Ninghuis-MBP.attlocal.net>
2023-10-08 12:07:05 -0700

    Cart total

--- cart/views.py
+++ cart/views.py
@@ -38,7 +38,7 @@ class ProductDetailView(generic.FormView):
             )
         if item_filter.exists():
             item = item_filter.first()
-            item.quantity = int(form.cleaned_data['quantity'])
+            item.quantity += int(form.cleaned_data['quantity'])
             item.save()
         else:
             new_item = form.save(commit=False)
